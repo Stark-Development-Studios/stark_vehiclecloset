38a16c41d37549525eb01609387fa85329709df6
Adam Biegert <theadman2092@ymail.com>
2025-11-22 00:24:26 -0700

    update(stark_vehiclecloset): Update To Organization

--- server/main.lua
+++ server/main.lua
@@ -3,5 +3,5 @@ if not lib.checkDependency('ox_lib', '3.32.0', true) then return end
 local Config = require 'shared.config'
 
 if Config.VersionCheck then
-    lib.versionCheck('AdamaStark-N7/stark_vehiclecloset')
+    lib.versionCheck('Stark-Development-Studios/stark_vehiclecloset')
 end
--- shared/config.lua
+++ shared/config.lua
@@ -1,8 +1,12 @@
 return {
     VersionCheck = true,
+
     VehicleKeysRequired = true,
-    Framework = 'qbx',    -- supported: 'qb' or 'qbx'
-    Target = 'ox',        -- supported: 'qb' or 'ox'
+
+    Framework = 'qbx', -- supported: 'qb' or 'qbx'
+
+    Target = 'ox',     -- supported: 'qb' or 'ox'
+
     Progress = {
         style = 'ox_bar', -- supported: 'ox_bar' or 'ox_circle'
         duration = 2000
